40a673896b8a3129637bfbe4d2ecce06e18be93e
Tobias Bühlmann <tobias.buehlmann@gmx.de>
2013-04-14 22:39:09 +0200

    Synchronizing User methods.

--- lib/vetinari/channel_container.rb
+++ lib/vetinari/channel_container.rb
@@ -14,20 +14,24 @@ module Vetinari
     end
 
     def [](channel_or_channel_name)
-      case channel_or_channel_name
-      when Channel
-        if @channels.include?(channel_or_channel_name)
-          channel_or_channel_name
-        end
-      when String
-        @channels.find do |c|
-          c.name.downcase == channel_or_channel_name.downcase
+      @mutex.synchronize do
+        case channel_or_channel_name
+        when Channel
+          if @channels.include?(channel_or_channel_name)
+            channel_or_channel_name
+          end
+        when String
+          @channels.find do |c|
+            c.name.downcase == channel_or_channel_name.downcase
+          end
         end
       end
     end
 
     def has_channel?(channel)
-      self[channel] ? true : false
+      @mutex.synchronize do
+        self[channel] ? true : false
+      end
     end
 
     def remove(channel)
@@ -66,13 +70,15 @@ module Vetinari
     # Returns a Set of all Users that are in one of the Channels from the
     # ChannelList.
     def users
-      users = Set.new
+      @mutex.synchronize do
+        users = Set.new
 
-      @channels.each do |channel|
-        users.merge(channel.users)
+        @channels.each do |channel|
+          users.merge(channel.users)
+        end
+
+        users
       end
-
-      users
     end
   end
 end
