4ceed1de81b6ea26fb2e8206bd85b9162d268a0a
Tobias Bühlmann <tobias.buehlmann@gmx.de>
2013-05-13 22:14:23 +0200

    Fixed typo in MessageParser.

--- lib/vetinari/message_parser.rb
+++ lib/vetinari/message_parser.rb
@@ -21,7 +21,7 @@ module Vetinari
       when /^:(\S+)!(\S+)@(\S+) MODE ((?:#{chantypes})\S+) ([+-]\S+)/
         {:type => :channel_mode, :nick => $1, :user => $2, :host => $3, :channel => $4, :modes => $5, :params => $'.lstrip}
       when /^:(\S+)!(\S+)@(\S+) NICK :/
-        {:type => :nickchange, :nick => $1, :user => $2, :host => $3, :new_nick => $'}
+        {:type => :nick_change, :nick => $1, :user => $2, :host => $3, :new_nick => $'}
       when /^:(\S+)!(\S+)@(\S+) KICK (\S+) (\S+) :/
         {:type => :kick, :nick => $1, :user => $2, :host => $3, :channel => $4, :kickee => $5, :message => $'}
       when /^:(\S+)!(\S+)@(\S+) TOPIC (\S+) :/
